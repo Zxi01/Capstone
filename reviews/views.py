ec9b06b703281b07a8cdf5ac593d2fe134871f23
Zxi01 <cameron.m.jennings@hotmail.com>
2025-11-26 15:38:10 +0000

    crispy form to add comments and wait for approval

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
 import json
 import os
 from django.conf import settings
 from django.utils.text import slugify
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -75,6 +77,20 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
+
     return render(
         request,
         "reviews/post_detail.html",
@@ -82,6 +98,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
-    )   
-
+    )
